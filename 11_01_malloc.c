6f2eeb118b222c4aa2645c4ee9a0e73c6aa65f97
Nikhil Dhariwal <nicksd111@gmail.com>
2022-09-19 22:33:53 +0530

    Update 11_01_malloc.c

--- 11_01_malloc.c
+++ 11_01_malloc.c
@@ -3,10 +3,6 @@
 
 int main(){
     int *ptr;
-    // Sizeof operator in C
-    // printf("The size of int on my pc is %d\n", sizeof(int));
-    // printf("The size of float on my pc is %d\n", sizeof(float));
-    // printf("The size of char on my pc is %d\n", sizeof(char));
     ptr = (int *) malloc(6 * sizeof(int)); 
     for(int i=0; i<6;i++){
         printf("Enter the value of %d element: \n", i); 
